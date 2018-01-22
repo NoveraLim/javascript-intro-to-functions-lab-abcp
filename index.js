443163e6104f80308f2cda32fa79aba382a5cbfd
Novera Lim <lim.novera88@gmail.com>
2018-01-22 19:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function logWhisper(string) {
+function shout(string) {
+   return string.toUpperCase()
+ }
+ 
+ function whisper(string) {
+   return string.toLowerCase()
+ }
+ 
+ function logShout(string) {
+   console.log(string.toUpperCase())
+ }
+ 
+ function logWhisper(string) {
    console.log(string.toLowerCase())
  }
+
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {
   return "I can't hear you!"
@@ -10,4 +23,5 @@ if (string.toUpperCase() === string) {
 }
 if (string === "I love you, Grandma.") {
   return "I love you, too."
+}
 }
